3f9dced5cda10be98e058f86466e758cc78ac28c
Maks Golovin <70146981+drgon-dev@users.noreply.github.com>
2023-05-29 17:57:23 +0500

    Add files via upload

---
+++ Классы12.1/Main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <set>
+using namespace std;
+typedef multiset<float> TMS;
+typedef TMS::iterator it;
+
+TMS make_set(int n) {
+	srand(time(NULL));
+	TMS s;
+	float a;
+	for (int i = 0; i < n; i++) {
+		a = round(((float)(rand()) / RAND_MAX * 10 + 1) * 100) / 100;
+		s.insert(a);
+	}
+	return s;
+}
+
+void print_TMS(TMS m) {
+	int k = 1;
+	for (it i = m.begin(); i != m.end(); i++)
+		cout << k++ << " : " << *i << " " << endl;
+}
+
+void Add_Mid_Beg(TMS v) {
+	float mid; int i = 0;
+	for (auto it = v.begin(); it != v.end(); ++it) {
+		mid = mid + *it;
+		i++;
+	}
+	v.emplace(v.begin(), mid);
+}
+
+void Find_Ers(TMS v, float key) {
+	for (auto it = v.begin(); it != v.end(); ++it) {
+		if (*it == key) {
+			it = v.erase(it);
+		}
+	}
+}
+
+void Min_Elem(TMS v) {
+	float min = 1000; TMS m;
+	for (auto it = v.begin(); it != v.end(); ++it) {
+		if (min < *it) {
+			min = *it;
+		}
+	}
+	for (it i = v.begin(); i != v.end(); ++i) {
+		float temp = *i - min;
+		m.insert(temp);
+	}
+	v = m;
+}
+
+int main() {
+	TMS ms;
+	ms = make_set(5);
+	print_TMS(ms);
+	return 0;
+}
